c8f9bcf766d2d90cb74e5c3eb7e4847edcc6f06e
Sazida Hossain <sazidahossain@Sazidas-MacBook-Pro.local>
2018-10-07 13:51:18 +0300

    Task Submission

--- restaurants/apps.py
+++ restaurants/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class RestaurantsConfig(AppConfig):
     name = 'restaurants'
+    
+    def home(request):
+    	context = {
+        "msg": "Hello World!"}
+    	return render(request, 'template1.html',context)
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+	context = {
+	'msg': "Hello World!"}
+	return render(request, 'template1.html',context)
